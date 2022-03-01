4445fd1ab159cc5ab5fab6c7f8705c50a020ac0d
mk-hasan <mkhasan6891@gmail.com>
2022-03-01 09:25:56 +0600

    sensor and others information added

--- js/phone-explore.js
+++ js/phone-explore.js
@@ -13,7 +13,7 @@ const searchPhone = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`;
         fetch(url)
             .then(response => response.json())
-            .then(data => displaySearchResult(data.data))
+            .then(data => displaySearchResult(data.data.slice(0, 20)))
         searchField.value = '';
     }
 }
@@ -21,7 +21,7 @@ const searchPhone = () => {
 
 const displaySearchResult = (datas) => {
     // console.log(datas)
-    searchResult.innerHTML = "";
+    searchResult.innerHTML = ""; // empty previous all phonde preview result
     datas.forEach(data => {
         // console.log(data)
         const div = document.createElement('div');
@@ -51,17 +51,27 @@ const loadPhoneDetail = (phoneId) => {
 }
 const displayPhoneDetail = (data) => {
     console.log(data);
-    phoneDetails.innerHTML = "";
+    phoneDetails.innerHTML = ""; // empty previous single phonde details preview
     const div = document.createElement('div');
-    div.classList.add('card');
+    div.classList.add('col', 'card');
     div.innerHTML = `
         <img src="${data.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${data.name}</h5>
                 <p class="card-text">Release Date: ${data.releaseDate}</p>
-                <p class="card-text">Main Features: ${data.mainFeatures.chipSet}</p>
-                <p class="card-text">${data.mainFeatures.memory}</p>
-                <p class="card-text">${data.mainFeatures.displaySize}</p>
+                <h6 class="card-text">Main Features:</h6>
+                <p class="card-text">Chipset: ${data.mainFeatures.chipSet}</p>
+                <p class="card-text">Memory: ${data.mainFeatures.memory}</p>
+                <p class="card-text">Display Size: ${data.mainFeatures.displaySize}</p>
+                <h6 class="card-text">Sensors:</h6>
+                <p class="card-text">${data.mainFeatures.sensors[0]}, ${data.mainFeatures.sensors[1]}, ${data.mainFeatures.sensors[2]}, ${data.mainFeatures.sensors[3]}, ${data.mainFeatures.sensors[4]}, ${data.mainFeatures.sensors[5]}</p>
+                <h6 class="card-text">Others Features:</h6>
+                <p class="card-text">Bluetooh: ${data.others.Bluetooth}</p>
+                <p class="card-text">USB: ${data.others.USB}</p>
+                <p class="card-text">GPS: ${data.others.GPS}</p>
+                <p class="card-text">NFC: ${data.others.NFC}</p>
+                <p class="card-text">Radio: ${data.others.Radio}</p>
+                <p class="card-text">WLAN: ${data.others.WLAN}</p>
                 <p class="card-text">ID: ${data.slug}</p>
             </div>
     `;
